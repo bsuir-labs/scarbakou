5aba17a20d137502fe66277b706b3fbf59fe0ad1
bsuir-labs@yandex.by <kickoman@yandex.by>
2020-05-03 14:44:05 +0300

    fix #2 Comments in main

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -19,40 +19,45 @@ void printArr(const std::string &name, const CustomArray &arr)
 int main()
 {
     srand(time(nullptr));
-    CustomArray array;
 
+    // Example of array resizing
+    CustomArray array;
     array.resize(10);
     printArr(array);
 
+
+    // Exmaple of elements value assigning
     for (unsigned i = 0; i < array.size(); ++i)
         array[i] = i * 2;
-
     printArr(array);
 
+
+    // Example of array resizing
     array.resize(14);
     printArr(array);
     array.resize(4);
     printArr(array);
+
+    // Example of array sorting
     array.resize(20);
     printArr(array);
-
     for (unsigned i = 0; i < array.size(); ++i)
         array[i] = rand() % 50 - 20;
-
     printArr(array);
 
     array.sort();
-
     printArr(array);
 
+
+    // Example of inserting element into sorted array
     array.insertKeepSorted(12);
-
     printArr(array);
 
+
+    // Example of merging sorted arrays
     CustomArray second(20);
     for (unsigned i = 0; i < second.size(); ++i)
         second[i] = rand() % 50 - 20;
-
     printArr(second);
 
     second.sort();
@@ -63,10 +68,12 @@ int main()
 
     printArr(merged);
 
-    CustomArray crossed = CustomArray::Cross(array, second);
 
+    // Example of crossing sorted arrays
+    CustomArray crossed = CustomArray::Cross(array, second);
     printArr(crossed);
 
+    // Example of searching in sorted array
     for (unsigned i = 0; i < 40; ++i)
     {
         int el = rand() % 50 - 20;
